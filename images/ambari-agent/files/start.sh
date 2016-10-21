e3aa266e1a20a0ad0a759491733e6971302daf1f
root <root@master.iop.com>
2016-10-21 14:56:58 +0800

    change shmmax when linux kernel less than 3.16

--- images/ambari-agent/files/start.sh
+++ images/ambari-agent/files/start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#config shmmax
+kernel_version=$(uname -r)
+echo $kernel_version
+if [ "$kernel_version" \< "3.16.0" ]; then
+   echo 1073741824 > /proc/sys/kernel/shmmax
+fi
+
 service sshd start
 sed s/AMBARI_SERVER_HOSTNAME/$AMBARI_SERVER_HOSTNAME/ /etc/ambari-agent/conf/ambari-agent.ini.template > /etc/ambari-agent/conf/ambari-agent.ini
 ambari-agent start >>/dev/null
--- images/ambari-server/files/start.sh
+++ images/ambari-server/files/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#config shmmax
+kernel_version=$(uname -r)
+echo $kernel_version
+if [ "$kernel_version" \< "3.16.0" ]; then
+   echo 1073741824 > /proc/sys/kernel/shmmax
+fi
 ambari-server start >>/dev/null
 while true; do sleep 1000; done
